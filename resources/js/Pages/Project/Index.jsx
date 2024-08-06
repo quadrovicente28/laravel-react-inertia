12e524e6280495687976076ec736d13d6b4c29c1
quadrovicente28 <118233540+quadrovicente28@users.noreply.github.com>
2024-08-07 02:25:11 +0800

    Initial commit

---
+++ resources/js/Pages/Project/Index.jsx
@@ -0,0 +1,28 @@
+
+
+import { Head } from "@inertiajs/react";
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+import React from 'react'
+
+export default function Index({auth, projects}) {
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+          Projects
+        </h2>
+      }
+    >
+          <Head title="Projects" />
+          
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900 dark:text-gray-100">Projects</div>
+          </div>
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
+}
